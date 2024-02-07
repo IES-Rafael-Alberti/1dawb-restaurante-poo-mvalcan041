0591c106eb87b3abce4702967e20838316c47e2f
mvalcan041 <mvalcan041@g.educaand.es>
2024-02-07 17:20:42 +0100

    Added: Plato

--- POO-Restaurante/src/main/kotlin/Plato.kt
+++ POO-Restaurante/src/main/kotlin/Plato.kt
@@ -1,9 +1,27 @@
-class Plato(var nombre: String, var precio: Double, var tiempoPreparacion: Int, val ingredientes: MutableList<String> = mutableListOf()) {
+class Plato(nombre: String, precio: Double, tiempoPreparacion: Int, val ingredientes: MutableList<String> = mutableListOf()) {
+
+    var nombre: String = nombre
+        set(value) {
+            requireNombre(nombre)
+            field = value
+        }
+    var precio: Double = precio
+        set(value) {
+            field = value
+        }
+    var tiempoPreparacion: Int = tiempoPreparacion
+        set(value) {
+            field = value
+        }
 
     init {
-        require(nombre.isNotEmpty()) { "El nombre del plato no puede estar vacío." }
+        requireNombre(nombre)
         require(precio > 0) { "El precio del plato debe ser mayor que 0." }
         require(tiempoPreparacion > 1) { "El tiempo de preparación no podrá ser igual o inferior a 1." }
+    }
+
+    private fun requireNombre(nombre) {
+        require(nombre.isNotEmpty()) { "El nombre del plato no puede estar vacío." }
     }
 
     fun agregarIngrediente(ingrediente: String) {
